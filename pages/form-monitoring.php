8b82b7e8f0068ca76c524d0ad92832ab33519576
Nktpamungkas <nktpamungkas28@gmail.com>
2024-02-26 13:19:05 +0700

    ubah jam masuk kain menjadi format 24 jam

--- pages/form-monitoring.php
+++ pages/form-monitoring.php
@@ -75,7 +75,6 @@
 	}
 ?>
 <?php
-	log()
 	$sqlc = "select convert(char(10),CreateTime,103) as TglBonResep,convert(char(10),CreateTime,108) as JamBonResep,ID_NO,COLOR_NAME,PROGRAM_NAME,PRODUCT_LOT,VOLUME,PROGRAM_CODE,YARN as NoKK,TOTAL_WT,USER25 from ticket_title where ID_NO='" . $rcek['no_resep'] . "' order by createtime Desc";
 	//--lot
 	$qryc = sqlsrv_query($conn1, $sqlc, array(), array("Scrollable" => "static"));
@@ -306,13 +305,22 @@
 			<div class="col-md-6">
 				<div class="form-group">
 					<label for="no_po" class="col-sm-3 control-label">No KK</label>
-					<div class="col-sm-3">\
+					<div class="col-sm-3">
 						<input name="nokk" type="text" class="form-control" id="nokk" onchange="window.location='?p=Form-Monitoring&nokk='+this.value" value="<?php echo $_GET['nokk']; ?>" placeholder="No KK" required>
 						<input name="id" type="hidden" class="form-control" id="id" value="<?php echo $rcek['id']; ?>" placeholder="ID">
 					</div>
 					<label for="jammasukkain" class="col-sm-2 control-label">Jam Masuk Kain</label>
-					<div class="col-sm-3">
-						<input name="jammasukkain" type="datetime-local" class="form-control" id="jammasukkain">
+					<div class="col-sm-2">
+						<input name="jammasukkain" type="date" class="form-control col-sm-2">
+						</div>
+					<div class="col-sm-2">
+						<input name="tglmasukkain" type="text" class="form-control col-sm-2" id="tglmasukkain" placeholder="00:00" pattern="[0-9]{2}:[0-9]{2}$" title=" e.g 14:25" onkeyup="
+																				var time = this.value;
+																				if (time.match(/^\d{2}$/) !== null) {
+																					this.value = time + ':';
+																				} else if (time.match(/^\d{2}\:\d{2}$/) !== null) {
+																					this.value = time + '';
+																				}" value="<?php echo $rw['jam_in'] ?>" size="5" maxlength="5">
 					</div>
 				</div>
 				<div class="form-group">
@@ -1250,7 +1258,7 @@ if ($_POST['save'] == "save") {
 		kategori_resep = '$_POST[kategori_resep]',
 		kasih_resep = '$_POST[kasih_resep]',
 		acc_resep = '$_POST[acc_resep]',
-		jammasukkain = '$_POST[jammasukkain]',
+		jammasukkain = '$_POST[jammasukkain] $_POST[tglmasukkain]',
 		tgl_update=now()");
 
 	if ($sqlData) {
